ee2336385e05bdeef65bcc0c8c0a3bb4568ed8c5
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-01-28 02:47:08 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function toReadable (number) {
     var d = " ";
-    var units = {1: "one", 2: "two", 3: "three", 4:"four", 5: "five", 6: "six", 7: "seven", 8: "eigth",9: "nine"}
+    var units = {'1': "one", '2': "two", '3': "three", '4':"four", '5': "five", '6': "six", '7': "seven", '8': "eigth",'9': "nine"}
     var tens = {10: "ten",20: "twenty", 30: "thirty", 40: "forty", 50: "fifty",60: "sixty", 70: "seventy",80: "eigthy", 90: "ninety"}
     var teens = {11: "eleven", 12: "twelve", 13:"thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18:"eigthteen", 19:"nineteen"}
         if(Math.ceil(number/100) == 1){
