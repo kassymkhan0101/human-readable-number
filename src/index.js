483b393f0e2c44605c3bcf8c2c1642e6662dc93c
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-01-28 17:25:31 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,32 +30,7 @@ module.exports = function toReadable (number) {
              }
          }
      }
-    else if(num.length == 3){
-        for(i=1; i<10; i++){
-            if(num[0]==i && num[1]==0){
-                for(k=1; k<10; k++){
-                            if(num[2]==k){
-                                return (units[i]+" "+"hundred"+" "+ units[k]);
-                     }
-                 }
-             }
-         }
-            
-        
-    }
-    else if(num.length == 3){
-        for(i=1; i<10; i++){
-            if(num[0]==i){
-                for(u=2; u<10; u++){
-                    if(num[1]==u && num[2]==0){
-                         return (units[i]+" "+"hundred"+" "+tens[u-1]);
-                     }
-                 }
-             
-         
-            }
-        }
-    }
+    
     else if(num.length == 3){
         for(i=1; i<10; i++){
             if(num[0]==i){
@@ -64,6 +39,8 @@ module.exports = function toReadable (number) {
                          for(k=1; k<10; k++){
                             if(num[2]==k){
                                 return (units[i]+" "+"hundred"+" "+tens[u-1]+" "+ units[k]);
+                     }else if(num[2]==0){
+                         return (units[i]+" "+"hundred"+" "+tens[u-1]);
                      }
                  }
              }
