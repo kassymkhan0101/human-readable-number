20996f6d0e4bf3adae935384663ca098bf1e34f7
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-01-28 14:42:48 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 module.exports = function toReadable (number) {
+    var d = "";
     var n = number.toString();
     var units = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen","eighteen", "nineteen"];
-    var tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy","eighty", "ninety"];
+    var tens = ["", "twenty", "thirty", "forty", "fifty", "sixty", "seventy","eighty", "ninety"];
     if(n<20){for(i=0; i<20; i++){
         if(n == i){
             return units[i];
         }
-    }
-    if(n>20 && n<100){
-        
-    for(j=0; j< 8; j++){
-        if(n.charAt(0)== j+2){
-            for(k=0; k<10; k++){
-            if(n.charAt(1) == k+1){
-                return (tens[j] + units[k+1]);
-                
-            }
-        }
-    }
-  
-    
-}}}
+    }else if(n==20){
+        return tens[1]
+    }else if(n>20 && n<100){
+        for(i=0; i<9; i++){
+        if(n.charAt(0)== i){
+            d = d + tens[i]
+        for(i=0; i<9; i++){
+             if(n.charAt(1)== i+1){
+                 d = d + units[i+1]
+                 return d
+        }}
+    }}
+   }
