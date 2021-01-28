eb08573fa1a9a8a9da15f385e5f0b9dfae439e00
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-01-28 15:14:19 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 module.exports = function toReadable (number) {
     var units = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen","eighteen", "nineteen"];
     var tens = ["", "twenty", "thirty", "forty", "fifty", "sixty", "seventy","eighty", "ninety"];
+    for(i=2; i<10; i++){
+        if(num/10==i){
+            return tens[i-1];
+        }
+    }
     var num = number.toString().split("");
     if(num.length == 1){
         for(i=0; i<10; i++){
